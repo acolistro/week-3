740af0f2c51338472d09ad08bd6785bb01a2f802
Alyssa Colistro <arcolistro@gmail.com>
2019-11-01 15:45:48 -0700

    consolidate code, now getting a simple syntax error. created brach of previous commit just in case.

--- js/scripts.js
+++ js/scripts.js
@@ -4,41 +4,23 @@ $(document).ready(function() {
     //Gather input from user (number)
     var number = $("input#number").val();
     var numberInput = parseInt(number);
-    var numberArray = [];
+  //  var numberArray = [];
 
     //Count from zero to inputted number and push it into array
     for (var currentNumber = 0; currentNumber <= numberInput; currentNumber += 1) {
-      numberArray.push(toString(currentNumber));
+      //numberArray.push(toString(currentNumber));
+      toString(currentNumber);
+      function iterate(currentNumber) {
+          if (currentNumber.includes(1)) {
+            $("ul").append("<li>Beep!</li>");
+          } else if (currentNumber.includes(2)) {
+            $("ul").append("<li>Boop!</li>");
+          } else if (currentNumber.includes(3)) {
+            $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>");
+          } else {
+            $("ul").append("<li>" + currentNumber + "</li>");
+          }
     }
-
-    //Turn each array item into a string and output it
-
-  /*  function numberOutput(numberInput) {
-      numberInput = toString(numberInput);
-      if (numberInput.includes(1)) {
-        $("ul").append("<li>Beep!</li>");
-      } else if (numberInput.includes(2)) {
-        $("ul").append("<li>Boop!</li>");
-      } else if (numberInput.includes(3)) {
-        $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
-      } else {
-        $("ul").append("<li>" + numberInput + "</li>")
-      }
-    }*/
-
-
-    numberArray.forEach(function(item) {
-        if (item.includes(1)) {
-          $("ul").append("<li>Beep!</li>");
-        } else if (item.includes(2)) {
-          $("ul").append("<li>Boop!</li>");
-        } else if (item.includes(3)) {
-          $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
-        } else {
-          $("ul").append("<li>" + item + "</li>")
-        }
-      });
-
 
     $("#main").hide();
     $("#output").show();
@@ -47,6 +29,33 @@ $(document).ready(function() {
   });
 });
 
+//Turn each array item into a string and output it
+
+/*  function numberOutput(numberInput) {
+  numberInput = toString(numberInput);
+  if (numberInput.includes(1)) {
+    $("ul").append("<li>Beep!</li>");
+  } else if (numberInput.includes(2)) {
+    $("ul").append("<li>Boop!</li>");
+  } else if (numberInput.includes(3)) {
+    $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
+  } else {
+    $("ul").append("<li>" + numberInput + "</li>")
+  }
+}*/
+
+
+/*numberArray.forEach(function(item) {
+    if (item.includes(1)) {
+      $("ul").append("<li>Beep!</li>");
+    } else if (item.includes(2)) {
+      $("ul").append("<li>Boop!</li>");
+    } else if (item.includes(3)) {
+      $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
+    } else {
+      $("ul").append("<li>" + item + "</li>")
+    }
+  });*/
 
     //$("ul.unstyled").append("<li>" + numberInput + "</li>");
 
