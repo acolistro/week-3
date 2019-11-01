6cf18e5b767b0c48e3530e9756b725d309853c6a
Alyssa Colistro <arcolistro@gmail.com>
2019-11-01 10:42:55 -0700

    Add scripts to parse user input to number, run a function on it and parse it back to a string to allow required output to be correct, incomplete.

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,20 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var num = parseInt($("input#number").val());
+    var numberInput = parseInt($("input#number").val());
 
-  for (var currentNumber = 0; currentNumber <= num; currentNumber += 1) {
-    num += currentNumber;
+  for (var currentNumber = 0; currentNumber <= numberInput; currentNumber += 1) {
+    numberInput += currentNumber;
+    var numberOutput = numberInput.toString();
   }
 
   var count = function(num) {
-    if (num.includes("1") === true) {
-
+    if (numberOutput.includes("1") === true) {
+      $("ul").append("<li>Beep!</li>");
+    } else if (numberOutput.includes("2") === true) {
+      $("ul").append("<li>Boop!</li>");
+    } else if (numberOutput.includes("3") === true) {
+      $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
     }
 
   }
