d5cfa3610975f35cfdd1d12d891e820e11d491c6
Alyssa Colistro <arcolistro@gmail.com>
2019-11-01 10:23:08 -0700

    Edit and slight reformatting of scripts in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
+//Business logic
+
+
+//User interface logic
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    var numIntput = x
-    var num = x
+    event.preventDefault();
+    var num = parseInt($("input#number").val());
 
   for (var currentNumber = 0; currentNumber <= num; currentNumber += 1) {
     num += currentNumber;
   }
 
+  var count = function(num) {
+    if (num.includes("1") === true) {
+
+    }
+
+  }
+
   $("#main").hide();
   $("#output").show();
 
-  event.preventDefault();
+
   });
 });
