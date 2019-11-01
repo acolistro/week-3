09073ce0276fb6857688968ddafe30bf16b08618
Alyssa Colistro <arcolistro@gmail.com>
2019-11-01 14:04:20 -0700

    Edit scripts in effort to continue debugging.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,42 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
+
+    //Gather input from user (number)
     var number = $("input#number").val();
     var numberInput = parseInt(number);
     var currentNumber = 0;
     var numberArray = [];
 
+    //Count from zero to inputted number and push it into array
     while (currentNumber <= numberInput) {
       currentNumber += 1;
-      var numberOutput = numberInput += currentNumber;
-      numberArray.push(numberOutput);
+      numberInput += currentNumber;
+      numberArray.push(numberInput);
     }
 
-    numberArray.forEach(function (num) {
-      if (num.includes(1)) {
-        $("ul").append("<li>Beep!</li>");
-      } else if (num.includes(2)) {
-        $("ul").append("<li>Boop!</li>");
-      } else if (num.includes(3)) {
-        $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
-      } else {
-        $("ul").append("<li>" + num + "</li>")
-      }
-    });
+    numberArray.forEach(numberInput) {
+      numberInput = toString(numberInput);
+    }
+    //var numberOutput = toString(numberInput);
+
+
+    if (numberInput.includes(1)) {
+      $("ul").append("<li>Beep!</li>");
+    } else if (numberInput.includes(2)) {
+      $("ul").append("<li>Boop!</li>");
+    } else if (numberInput.includes(3)) {
+      $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
+    } else {
+      $("ul").append("<li>" + numberInput + "</li>")
+    }
+
+    $("#main").hide();
+    $("#output").show();
+
+    event.preventDefault();
+  });
+});
+
 
     //$("ul.unstyled").append("<li>" + numberInput + "</li>");
 
@@ -45,12 +60,3 @@ $(document).ready(function() {
     } else {
       $("ul").append("<li>" + listItemOutput + "</li>")
     }*/
-
-
-
-    $("#main").hide();
-    $("#output").show();
-
-    event.preventDefault();
-  });
-});
