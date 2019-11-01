334f14c44e262cea8692737af0e24412a30cc460
Alyssa Colistro <arcolistro@gmail.com>
2019-11-01 09:57:13 -0700

    Edit scripts.js with some code to count from zero to inputted number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
+    var numIntput = x
+    var num = x
 
+  for (var currentNumber = 0; currentNumber <= num; currentNumber += 1) {
+    num += currentNumber;
+  }
 
- 
+  $("#main").hide();
   $("#output").show();
 
   event.preventDefault();
-  })
-})
+  });
+});
