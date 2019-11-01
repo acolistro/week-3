bcfec4630ebe9e60530fba880d78d365ebd6f6a7
Alyssa Colistro <arcolistro@gmail.com>
2019-11-01 13:14:14 -0700

    Show debugging, appempt to use array again, site keeps crashing from current scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    var numberInput = parseInt($("input#number").val());
+    var number = $("input#number").val();
+    var numberInput = parseInt(number);
+    var currentNumber = 0;
+    var numberArray = [];
 
+    while (currentNumber <= numberInput) {
+      currentNumber += 1;
+      var numberOutput = numberInput += currentNumber;
+      numberArray.push(numberOutput);
+    }
+
+    numberArray.forEach(function (num) {
+      if (num.includes(1)) {
+        $("ul").append("<li>Beep!</li>");
+      } else if (num.includes(2)) {
+        $("ul").append("<li>Boop!</li>");
+      } else if (num.includes(3)) {
+        $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
+      } else {
+        $("ul").append("<li>" + num + "</li>")
+      }
+    });
+
+    //$("ul.unstyled").append("<li>" + numberInput + "</li>");
 
     /*var currentNumber = 0;
 
