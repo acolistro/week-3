cf133626095b81f7230f5821bc586dab352ff1fa
Alyssa Colistro <arcolistro@gmail.com>
2019-11-09 23:05:43 -0800

    Add stylings.

--- js/scripts.js
+++ js/scripts.js
@@ -31,15 +31,3 @@ $(document).ready(function() {
 
   });
 });
-
-    var listItemOutput = numberInput.numberOutput();
-
-    if (listItemOutput.includes("1")) {
-      $("ul").append("<li>Beep!</li>");
-    } else if (listItemOutput.includes("2")) {
-      $("ul").append("<li>Boop!</li>");
-    } else if (listItemOutput.includes("3")) {
-      $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
-    } else {
-      $("ul").append("<li>" + listItemOutput + "</li>")
-    }*/
