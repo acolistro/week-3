f8b8b1b214bd8d05cb29959831ad0770f4a081b3
Alyssa Colistro <arcolistro@gmail.com>
2019-11-09 22:39:20 -0800

    refactor code to fix bug.

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,36 @@
+var numberArray = [];
+
+function numberOutput(element) {
+  var outputString = element.toString();
+    if (outputString.includes(3)) {
+      $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>");
+    } else if (outputString.includes(2)) {
+      $("ul").append("<li>Boop!</li>");
+    } else if (outputString.includes(1)) {
+      $("ul").append("<li>Beep!</li>");
+    } else {
+      $("ul").append("<li>" + outputString + "</li>");
+    }
+  };
+
+
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var numberInput = parseInt($("input#number").val());
 
-    //Gather input from user (number)
-    var number = $("input#number").val();
-    var numberInput = parseInt(number);
-    var currentNumber = 0;
-  //  var numberArray = [];
+    for (i=0; i <= numberInput; i++) {
+      numberArray.push(i);
+      console.log(numberArray);
+    }
 
-    //Count from zero to inputted number and push it into array
-    for (currentNumber = 0; currentNumber <=  numberInput; currentNumber += 1) {
-      currentNumberString = currentNumber.toString();
-      //toString(currentNumber);
-      function iterate(currentNumberString) {
-        if (currentNumberString.includes(3)) {
-          $(".unstyled").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>");
-        } else if (currentNumberString.includes(2)) {
-            $(".unstyled").append("<li>Boop!</li>");
-          } else if (currentNumberString.includes(1)) {
-            $(".unstyled").append("<li>Beep!</li>");
-          } else {
-            $(".unstyled").append("<li>" + currentNumberString + "</li>");
-          }
-        }
-      }
+    numberArray.forEach(numberOutput);
 
     $("#main").hide();
     $("#output").show();
 
-    event.preventDefault();
   });
 });
-
-//Turn each array item into a string and output it
-
-/*  function numberOutput(numberInput) {
-  numberInput = toString(numberInput);
-  if (numberInput.includes(1)) {
-    $("ul").append("<li>Beep!</li>");
-  } else if (numberInput.includes(2)) {
-    $("ul").append("<li>Boop!</li>");
-  } else if (numberInput.includes(3)) {
-    $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
-  } else {
-    $("ul").append("<li>" + numberInput + "</li>")
-  }
-}*/
-
-
-/*numberArray.forEach(function(item) {
-    if (item.includes(1)) {
-      $("ul").append("<li>Beep!</li>");
-    } else if (item.includes(2)) {
-      $("ul").append("<li>Boop!</li>");
-    } else if (item.includes(3)) {
-      $("ul").append("<li>I'm sorry, Dave. I'm afraid I can't do that.</li>")
-    } else {
-      $("ul").append("<li>" + item + "</li>")
-    }
-  });*/
-
-    //$("ul.unstyled").append("<li>" + numberInput + "</li>");
-
-    /*var currentNumber = 0;
-
-    var numberOutput = function() {
-      while (currentNumber <= numberInput) {
-        currentNumber += 1;
-        numberInput += currentNumber;
-      }
-    }
 
     var listItemOutput = numberInput.numberOutput();
 
